c88982f01400949227edd06ead0241fb8b86ba3b
Guillaume <gui.lebas@gmail.com>
2019-03-14 14:20:15 +0100

    fix(commands/set) Fix undefined properties used to set entry

--- commands/set.js
+++ commands/set.js
@@ -3,13 +3,24 @@ const assign = require('lodash/fp/assign')
 const getEntry = require('../lib/entry/getEntry')
 const logReject = require('../lib/console/logReject')
 const setEntry = require('../lib/entry/setEntry')
+const setProp = require('../lib/collection/setProp')
 const slugify = require('../lib/slugify')
+
+// TODO: implement `List` (Traversable and Monoid ADT for iterables)
+// TODO: and/or implement transducer `toCompact` (eg. toCompact(List))
+// TODO: and/or implement `removeUndefined` utility (to remove undefined values
+//       from any iterable.
+const compactEntity = entity => Object.keys(entity).reduce(
+    (compacted, key) => entity[key] === undefined
+        ? compacted
+        : setProp(compacted, [key, entity[key]]),
+    {})
 
 /**
  * set :: Options -> Task Error Results
  */
 const set = ({ name, src, type, ...entity }, slug = slugify(entity.title)) =>
-    setEntry({ entity: assign({ slug }, entity), ...getEntry(src, '', type, name) })
+    setEntry({ entity: assign({ slug }, compactEntity(entity)), ...getEntry(src, '', type, name) })
         .orElse(logReject('There was an error while trying to set entry'))
 
 module.exports = set
