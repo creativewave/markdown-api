d8f54c8c96504515c4ccb538ff7ad14295cfe144
Guillaume <gui.lebas@gmail.com>
2019-04-29 11:49:31 +0200

    fix(commands/set) Refactorize with toCompact transducer

--- commands/set.js
+++ commands/set.js
@@ -2,24 +2,14 @@
 const getEntry = require('../lib/entry/getEntry')
 const logReject = require('../lib/console/logReject')
 const setEntry = require('../lib/entry/setEntry')
-const setProp = require('../lib/collection/setProp')
 const slugify = require('../lib/slugify')
-
-// TODO: implement `List` (Traversable and Monoid ADT for iterables)
-// TODO: and/or implement transducer `toCompact` (eg. toCompact(List))
-// TODO: and/or implement `removeUndefined` utility (to remove undefined values
-//       from any iterable.
-const compactEntity = entity => Object.keys(entity).reduce(
-    (compacted, key) => entity[key] === undefined
-        ? compacted
-        : setProp(compacted, [key, entity[key]]),
-    {})
+const toCompact = require('../lib/collection/toCompact')
 
 /**
  * set :: Options -> Task Error Results
  */
 const set = ({ name, src, type, ...entity }, slug = slugify(entity.title)) =>
-    setEntry({ entity: { ...compactEntity(entity), slug }, ...getEntry(name, { src, type }) })
+    setEntry({ entity: { ...toCompact(entity), slug }, ...getEntry(name, { src, type }) })
         .orElse(logReject('There was an error while trying to set entry'))
 
 module.exports = set
