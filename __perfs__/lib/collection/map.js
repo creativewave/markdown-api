ba922c2d1f708675c3975c5a491f509ff559428f
Guillaume <gui.lebas@gmail.com>
2020-01-29 09:56:38 +0100

    fix(__perfs__/lib/collection/map) Add perf unit test: map (vs lodash mapValues)

--- __perfs__/lib/collection/map.js
+++ __perfs__/lib/collection/map.js
@@ -1,15 +1,33 @@
 
 const _map = require('lodash/fp/map')
+const _mapValues = require('lodash/fp/mapValues')
 const map = require('../../../lib/collection/map')
 const benchmark = require('../../benchmark')
 
-const transform = i => i++
+const transformValue = i => ++i
+const transformEntry = e => [e[0], ++e[1]]
+const reduceEntry = (o, e) => {
+    o[e[0]] = transformValue(e[1])
+    return o
+}
+
+const createArray = () => [...Array(100)].map((_, i) => i)
+const createObject = () => createArray().reduce((o, i) => ({ ...o, [`prop-${i}`]: i }), {})
 
 benchmark({
-    name: 'map()',
+    name: 'map(transform, Array)',
     units: {
-        current: () => map(transform, [...Array(100)].map((_, i) => i)),
-        lodash: () => _map(transform, [...Array(100)].map((_, i) => i)),
-        native: () => [...Array(100)].map((_, i) => i).map(transform),
+        current: () => map(transformValue, createArray()),
+        lodash: () => _map(transformValue, createArray()),
+        native: () => createArray().map(transformValue),
+    },
+})
+
+benchmark({
+    name: 'map(transform, Object)',
+    units: {
+        current: () => map(transformEntry, createObject()),
+        lodash: () => _mapValues(transformValue, createObject()),
+        native: () => Object.entries(createObject()).reduce(reduceEntry, {}),
     },
 })
