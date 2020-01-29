a086e48d49414c2e4a8ac1aee2f2f77c522946ee
Guillaume <gui.lebas@gmail.com>
2020-01-29 09:55:01 +0100

    doc(commands/get) Fix terminology

--- commands/get.js
+++ commands/get.js
@@ -9,11 +9,11 @@ const get = () => 'Not implemented yet'
 
 // `api get --type <type> [--name <name] [...--<field>]`          should output contents, eg.:
 // -----------------------------------------------------------------------------
-// `api get entry  --type <EntryType>`                            should return raw content of all <EntryType>
-// `api get entry  --type <EntryType> --name <EntryName>`         should return raw content of <EntryName>
-// `api get entry  --type <EntryType> --name <EntryName> --title` should return raw title of <EntryName>
-// `api get entity --type <EntryType> --name <EntryName>`         should return processed content of <EntryName>
-// `api get entity --type <EntryType> --name <EntryName> --index` should only return processed index content of <EntryName>
-// `api get index  --type <EntryType> --name <IndexName>`         should return processed index contents of entries in <IndexName>
+// `api get entry  --type <EntityType>`                            should return raw content of all <EntityType>
+// `api get entry  --type <EntityType> --name <EntryName>`         should return raw content of <EntryName>
+// `api get entry  --type <EntityType> --name <EntryName> --title` should return raw title of <EntryName>
+// `api get entity --type <EntityType> --name <EntryName>`         should return processed content of <EntryName>
+// `api get entity --type <EntityType> --name <EntryName> --index` should only return processed index content of <EntryName>
+// `api get index  --type <EntityType> --name <IndexName>`         should return processed index contents of entries in <IndexName>
 
 module.exports = get
